a981eeeff3ef7054efb04dd0ac8f2137397cae18
topertz <k.markuci@gmail.com>
2025-04-10 15:35:29 +0200

    Add files via upload

---
+++ Controllers/HelpItem.cs
@@ -0,0 +1,7 @@
+public class HelpItem
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Category { get; set; }
+    public string? Image { get; set; }
+}
